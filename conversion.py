4a4de91bcb6bf8b65979eb09b73b3ffd9e9915d9
hamza malik <hamza.manzoor2626@gmail.com>
2025-07-29 12:49:42 +0500

    conversion file

---
+++ conversion.py
@@ -0,0 +1,35 @@
+num_int=10
+print('num_int', num_int)
+num_float=float(num_int)
+print('num_float:', num_float)
+gravity=45
+print(float(gravity))
+holmes=34.6
+print(int(holmes))
+fish=78
+print(str(fish))
+you='12.2'
+me=float(you)
+print(float(you))
+I=int(me)
+print(I)
+myname="hamza"
+print(myname)
+my_name_list=list(myname)
+print(my_name_list)
+
+firstname= 'hamza'
+print(firstname)
+print('Length of name:',len(firstname))
+
+print(min(10, 20, 50, 30, ))
+print(sum([1, 8 ,]))
+a, b =1, 2
+total = a + b
+Difference = a - b
+product = a*b
+division= a/b
+print('a + b = ', total)
+print('a - b=', Difference)
+print('a*b=', product)
+print('a/b=',division                )
