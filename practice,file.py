ee9cadc5e0c889f22cd1317fea48af13076099a9
hamza malik <hamza.manzoor2626@gmail.com>
2025-07-29 12:50:02 +0500

    practice file

---
+++ practice,file.py
@@ -0,0 +1,24 @@
+print(3 + 2) 
+print(3 * 2)
+print(3 ** 2)
+print(type(10))
+print(3 % 2)
+a = 5
+print(type(a))
+print(3 // 2)
+print("hello")
+len('hello')
+len("hello")
+print("Hello, World!")
+first_name, last_name, country, age, = 'Hamza', 'malik', 'pakistan', '21'
+print(first_name, last_name, country, age)
+print('first_name:', first_name)
+print('last_name:', last_name)
+print('country:', country)
+print('age:', age)
+
+first_name = input('what is your name?')
+age = input('how old are you?',)
+
+print('first_name:',first_name)
+print('age:',age)
