918cc1c70adea41a4d46a358f723d9aed35ad52b
hamza malik <hamza.manzoor2626@gmail.com>
2025-08-04 15:53:45 +0500

    oop day 3

--- OOP in python/oopday3.py
+++ OOP in python/oopday3.py
@@ -1,10 +1,21 @@
-class parent():
-    def father(self):
-        print("my father name  is Sajid")
+#composition in oop
+class Engine:
+    def start(self) -> str:
+        return"engine started"
+class Car:
+    def __init__(self):
+        self.engine = Engine()
 
-class son(parent):
-    def son(self):
-        print("my name is hamza.")
-c= son()
-c.son()
-c.father()
+    def drive(self) -> str:
+        return self.engine.start() + " | car is moving"
+my_car = Car()
+print(my_car.drive())
+
+
+class Ride:
+    def __init__(self):
+        self.engine = Engine()
+    def drive_ride(self) -> str:
+        return self.engine.start() + "|and riding it."
+rodeo = Ride()
+print(rodeo.drive_ride())
